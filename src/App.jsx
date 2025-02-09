f005ed8087b3c494aad481725c6a1fd2e103c47f
VanyaUzhorod <131401354+VanyaUzhorod@users.noreply.github.com>
2025-02-09 23:45:18 +0200

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,18 @@ import Header from "./components/Header/Header";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 // import MoviesPage from "./pages/MoviesPage/MoviesPage";
 import { Toaster } from "react-hot-toast";
-import MovieCast from "./components/MovieCast/MovieCast";
-import MovieReviews from "./components/MovieReviews/MovieReviews";
+// import MovieCast from "./components/MovieCast/MovieCast";
+// import MovieReviews from "./components/MovieReviews/MovieReviews";
 import { lazy, Suspense } from "react";
 
 const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
 const MoviesPage = lazy(() => import("./pages/MoviesPage/MoviesPage"));
 const MovieDetailsPage = lazy(() =>
   import("./pages/MovieDetailsPage/MovieDetailsPage")
+);
+const MovieCast = lazy(() => import("./components/MovieCast/MovieCast"));
+const MovieReviews = lazy(() =>
+  import("./components/MovieReviews/MovieReviews")
 );
 
 const App = () => {
